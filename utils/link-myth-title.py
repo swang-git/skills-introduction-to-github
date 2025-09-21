86faa228cbdfcb02de614c8dd3c103e58f28f4ba
swang-git <swang72@comcast.net>
2025-09-21 12:56:58 -0400

    reformat lnkmyth for non-exists record/file

--- utils/link-myth-title.py
+++ utils/link-myth-title.py
@@ -49,7 +49,7 @@ def getFullpath(basename):
     elif os.path.exists('/btv/' + basename): return '/btv/' + basename
     elif os.path.exists('/ctv/' + basename): return '/ctv/' + basename
     elif os.path.exists('/stv/' + basename): return '/stv/' + basename
-    elif os.path.exists('/ntv/' + basename): return '/ntv/' + basename
+    elif os.path.exists('/dtv/' + basename): return '/dtv/' + basename
 
 def getFullDirname(basename):
     if   os.path.exists('/home/swang/htv/' + basename): return '/home/swang/htv/'
@@ -57,7 +57,7 @@ def getFullDirname(basename):
     elif os.path.exists('/btv/' + basename): return '/btv/'
     elif os.path.exists('/ctv/' + basename): return '/ctv/'
     elif os.path.exists('/stv/' + basename): return '/stv/'
-    elif os.path.exists('/ntv/' + basename): return '/ntv/'
+    elif os.path.exists('/dtv/' + basename): return '/dtv/'
 
 def cleanupRecording():
     for idx, rec in enumerate(session.query(recorded).filter(recorded.watched == 2).order_by(recorded.starttime.desc())):
@@ -116,7 +116,7 @@ for idx, rec in enumerate(session.query(recorded).filter(recorded.watched == 0).
     recordedfile = getFullpath(rec.basename)
     if recordedfile == None:
         tit = fgcolor('yellow', rec.title)
-        if idx + 1 < limitNum: print('No recorded file with title=%s basename=%s starttime=%s'%(tit, rec.basename,stm))
+        if idx + 1 < limitNum: print('No recorded file with basename=%s title=%s starttime=%s'%(rec.basename, tit, stm))
         session.query(recorded).filter(recorded.basename == rec.basename).delete(synchronize_session=False)
         continue
 
