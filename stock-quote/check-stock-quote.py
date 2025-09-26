4706190fbd5c363d0a806456fad092ff15ce0875
swang-git <swang72@comcast.net>
2025-09-26 10:47:20 -0400

    enhanced more

--- stock-quote/check-stock-quote.py
+++ stock-quote/check-stock-quote.py
@@ -24,26 +24,21 @@ def reorder(rows) :
     data_sorted = sorted(rows, key=lambda o: order_map.get(o.symbol, len(symbols)))
     return data_sorted
 
-def get_quote_last_rows():
-    rows = dbsession(database).query(StockQuote).order_by(StockQuote.load_time.desc()).limit(6).all()
-    ordered_rows = reorder(rows)
-    # for row in ordered_rows: print(row.symbol)
-    # print(load_time.load_time)
-    return ordered_rows
+def get_stock_quotes(last_day=None):
+    if last_day == None: 
+        return dbsession(database).query(StockQuote).order_by(StockQuote.load_time.desc()).limit(6).all()
 
-def get_quote_last_2nd_rows(last_day):
-    rows2 = []
-    for add_days in [-1, -2, -3, -4]:
-        load_date = (datetime.now() + timedelta(days=add_days)).strftime('%Y-%m-%d')
+    backRange = 100
+    for add_days in range(backRange):
+        load_date = (datetime.now() + timedelta(days=-add_days)).strftime('%Y-%m-%d')
         if (load_date >= last_day): continue
         # print("load_date=%s last_day=%s comp=%s"%(load_date, last_day, load_date<=last_day))
-        rows2 = dbsession(database).query(StockQuote)\
+        rows = dbsession(database).query(StockQuote)\
             .filter(func.date_format(StockQuote.load_time, '%Y-%m-%d').label('formated_date')==load_date)\
             .order_by(StockQuote.load_time.desc()).limit(6).all()
-        if (len(rows2) == 6):
-            ordered_rows = reorder(rows2)
-            # for row in ordered_rows: print(row.symbol)
-            return ordered_rows
+        if (len(rows) == 6): return rows
+    print("There are not quotes for %d days back from %s"%(backRange, last_day))
+    sys.exit(1)
 
 def get_shares(): return {'T': 287, 'WBD': 69, 'CHTR':20, 'DELL':36, 'CSCO':640, 'MSFT':400}
 
@@ -80,20 +75,21 @@ def print_rows(rows, diff=None):
 
 def main():
     shares = get_shares()
-    rows1 = get_quote_last_rows()
+    rows = get_stock_quotes()
+    rows1 = reorder(rows)
     last_load_day = rows1[5].load_time.strftime('%Y-%m-%d')
     total1 = sum(row.price * shares[row.symbol] for row in rows1)
     # print("total1=%s"%total1)
     # for row in rows1: print("load_time=[%s] symbol=[%s]"%(row.load_time, row.symbol))
-    rows2 = get_quote_last_2nd_rows(last_load_day)
+    rows = get_stock_quotes(last_load_day)
+    rows2 = reorder(rows)
     total2 = sum(row.price * shares[row.symbol] for row in rows2)
     # print("total2=%s"%total2)
     diff = total1 - total2
+    diff = f'{diff:,.2f}'
+    diff = float(diff)
     print_rows(rows2)
-    # print(f"\033[1A\033[2K")
-    # print(' ║' + 128*' ' + '║')
     print_rows(rows1, diff)
     print(' ╚' + 128*'═' + '╝')
-    # print(diff)
 
 if __name__=="__main__": main()
