067ed21ae903428a3991bc2d7709acea3cd2fd19
swang-git <swang71@comcast.net>
2026-05-04 14:38:36 -0400

    re-colored glucose for HR-1/2/3

--- apps/glucosecheck/glulist.vue
+++ apps/glucosecheck/glulist.vue
@@ -435,9 +435,15 @@ function getClass (col, row) {
   else if (col === 'glucose' && between(row.glucose, 125, 141) && row.typeC === '空腹') return bgc + 'text-center text-green-5'
   else if (col === 'glucose' && between(row.glucose, 140, 156) && row.typeC === '空腹') return bgc + 'text-center text-blue'
   else if (col === 'glucose' && between(row.glucose, 155, 999) && row.typeC === '空腹') return bgc + 'text-center text-pink-4'
-  else if (col === 'glucose' && between(row.glucose,   0, 141) && row.typeC.slice(0, 1) === '餐') return bgc + 'text-center text-green'
-  else if (col === 'glucose' && between(row.glucose, 140, 201) && row.typeC.slice(0, 1) === '餐') return bgc + 'text-center text-amber'
-  else if (col === 'glucose' && between(row.glucose, 200, 999) && row.typeC.slice(0, 1) === '餐') return bgc + 'text-center text-pink-5'
+  else if (col === 'glucose' && between(row.glucose,   0, 156) && row.typeC.slice(0, 2) === '餐一') return bgc + 'text-center text-green'
+  else if (col === 'glucose' && between(row.glucose, 157, 199) && row.typeC.slice(0, 2) === '餐一') return bgc + 'text-center text-amber'
+  else if (col === 'glucose' && between(row.glucose, 200, 999) && row.typeC.slice(0, 2) === '餐一') return bgc + 'text-center text-pink-5'
+  else if (col === 'glucose' && between(row.glucose,   0, 156) && row.typeC.slice(0, 2) === '餐二') return bgc + 'text-center text-green'
+  else if (col === 'glucose' && between(row.glucose, 157, 199) && row.typeC.slice(0, 2) === '餐二') return bgc + 'text-center text-amber'
+  else if (col === 'glucose' && between(row.glucose, 200, 999) && row.typeC.slice(0, 2) === '餐二') return bgc + 'text-center text-pink-5'
+  else if (col === 'glucose' && between(row.glucose,   0, 156) && row.typeC.slice(0, 2) === '餐三') return bgc + 'text-center text-green'
+  else if (col === 'glucose' && between(row.glucose, 157, 199) && row.typeC.slice(0, 2) === '餐三') return bgc + 'text-center text-amber'
+  else if (col === 'glucose' && between(row.glucose, 200, 999) && row.typeC.slice(0, 2) === '餐三') return bgc + 'text-center text-pink-5'
   else return 'text-right'
 }
 // function isFasting (row) {
