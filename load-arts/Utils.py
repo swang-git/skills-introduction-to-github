2ba36cd822eaae03b48adbf8453b95afe87de3cb
swang-git <swang71@comcast.net>
2026-08-17 20:03:44 -0400

    port to mac local

--- load-arts/Utils.py
+++ load-arts/Utils.py
@@ -32,7 +32,7 @@ def wkdayname():
 
 
 def getLogFile(tag, dyx):
-    logFile = '/home/swang/tmp/logs/cn/load' + tag + \
+    logFile = '/Users/swang/tmp/logs/cn/load' + tag + \
         '_' + str(abs(dyx)) + '_' + wkdayname() + '.log'
     return logFile
 
@@ -220,14 +220,14 @@ class Img:
         #     # imgfile += hashlib.md5(lnk.encode('utf')).hexdigest() + '.png'
         # else: imgfile += imgfilename
 
-        savroot = "/sites/webdata"
+        savroot = "/Users/swang/sites/webdata"
         savpath = "/daily_data/" + self.tag + "/" + \
             str(self.year) + "/" + str(self.ymd) + "/images/"
         os.makedirs(savroot + savpath, exist_ok=True)
         savfile = savpath + imgfile
         dlout(4, 'QID', self.qid)
         if self.qid == 'testing':
-            tofile = "/home/swang/tmp/" + self.tag + "_" + self.ymd + "_" + imgfile
+            tofile = "/Users/swang/tmp/" + self.tag + "_" + self.ymd + "_" + imgfile
         else:
             tofile = savroot + savfile
 
