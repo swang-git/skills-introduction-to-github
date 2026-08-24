0dd1a1c2653b2d7e444a8881fed612929d1297de
swang-git <swang71@comcast.net>
2026-08-24 15:35:16 -0400

    coment out "calling from users"

--- apps/src/web.php
+++ apps/src/web.php
@@ -573,7 +573,7 @@ Route::post('/apps/loginAdmin', 'AppsController@loginAdmin');
 // Route::get('/users', [UserController::class, 'getUserList'])->name('users.getUserList');
 
 Route::group (
-  array('prefix' => 'users'), function() { Log::info('calling users from web.php');
+  array('prefix' => 'users'), function() { //Log::info('calling users from web.php');
     Route::get ('getUserList', 'UserController@getUserList')->middleware('auth');
     Route::get ('del/{id}', 'UserController@deleteById'); //->middleware('auth');
     Route::post('upd', 'UserController@updateUser'); //->middleware('auth');
