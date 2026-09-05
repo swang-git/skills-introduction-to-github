e75bc7eac1e558cb7e844504a9c53d0b2b660869
swang-git <swang71@comcast.net>
2026-09-05 13:26:29 -0400

    new .quasar file app.js

--- yali/.quasar/prod-spa/app.js
+++ yali/.quasar/prod-spa/app.js
@@ -28,6 +28,7 @@ export default async function (createAppFn, quasarUserOptions) {
   // Here we inject into it the Quasar UI, the router & possibly the store.
   const app = createAppFn(RootComponent)
 
+  app.config.performance = true
 
   app.use(Quasar, quasarUserOptions)
 
