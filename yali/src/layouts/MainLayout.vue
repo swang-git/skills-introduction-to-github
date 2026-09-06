1dcbd679665f2d0edb81967b220a62e2e443f436
swang-git <swang71@comcast.net>
2026-09-06 10:46:32 -0400

    add getPrev/NextPage buttons

--- yali/src/layouts/MainLayout.vue
+++ yali/src/layouts/MainLayout.vue
@@ -8,7 +8,7 @@
             <div v-if="ym!=null" class="text-center cursor-pointer text-whit q-pt-sm text-h4" @click="loadRandomPage">婭莉画展 <span class="text-h5">({{ ym }})</span>
               <q-tooltip class="text-h4 text-lime bg-red">跳转随机画页</q-tooltip>
             </div>
-            <div v-else class="text-center cursor-pointer text-whit q-pt-sm text-h4" @click="loadRandomPage">婭莉画展 <span class="text-h5">(共{{ total }}幅)</span>
+            <div v-else class="text-center cursor-pointer text-whit q-pt-sm text-h4" @click="loadRandomPage">婭莉画展 <span class="text-h6">(共{{ total }}幅)</span>
               <q-tooltip class="text-h4 text-lime bg-red">跳转随机画页</q-tooltip>
             </div>
             <RoundButton size="16px" icon="月" clas="q-pb-xs" colr="indigo-9" style="margin-top:5px" iclr="yellow" ttip="跳到某年某月" @click="openYmPad" />
@@ -54,11 +54,11 @@
           <q-card class="bg-teal-10" style="margin-top:10px">
             <q-card-actions align="between">
               <RoundButton v-if="isDesk" size="16px" icon="头" clas="q-pb-sm" colr="red-10" iclr="yellow" ttip="just show the first page" @click="getFirstPage" />
-              <RoundButton v-if="isIM" :dsbl="pageBegin<=1" size="16px" icon="first_page" colr="brown-9" iclr="yellow" ttip="show previous page" @click="getPrevPage" />
+              <RoundButton :dsbl="pageBegin<=1" size="16px" icon="first_page" colr="brown-9" iclr="yellow" ttip="show previous page" @click="getPrevPage" />
               <RoundButton size="16px" icon="chevron_left"  colr="indigo-10" iclr="yellow" ttip="show previous month" @click="getPrevYM" />
               <q-btn v-if="ym!=null" size="24px" :label="total"  colr="teal-10" />
               <RoundButton size="16px" icon="chevron_right" colr="indigo-10" iclr="yellow" ttip="show next mouth" @click="getNextYM" />
-              <RoundButton v-if="isIM" :dsbl="pageBegin>=lastPage" size="16px" icon="last_page" colr="brown-9" iclr="yellow" ttip="show next page" @click="getNextPage" />
+              <RoundButton :dsbl="pageBegin>=lastPage" size="16px" icon="last_page" colr="brown-9" iclr="yellow" ttip="show next page" @click="getNextPage" />
               <RoundButton v-if="isDesk" size="16px" icon="尾" clas="q-pb-sm" colr="red-10" iclr="yellow" ttip="just the last page" @click="getLastPage" />
             </q-card-actions>
           </q-card>
