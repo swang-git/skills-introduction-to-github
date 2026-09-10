3d219761e3b5f19078d0a0db617f81cdbf3a11da
swang-git <swang71@comcast.net>
2026-09-10 12:24:28 -0400

    option for upgrade all

--- utils/quasar-upgrade.sh
+++ utils/quasar-upgrade.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 
-[[ "$1" == "-h" ]] && echo "Usage: qupd check for checking quasar upgrade arts only" && exit
+[[ "$1" == "-h" ]] && echo "Usage: qupd check for checking quasar upgrade arts only; upd update all" && exit
 
 logFile="/Users/swang/tmp/qupd-arts.log"
 if [[ "$1" == "check" ]]; then
     cd ~/sites/projects/arts
+    echo "  ✅ -- Upgrading arts"
     quasar upgrade -i | tee $logFile 2>&1
 fi
 
-if [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
-else
-    cd /Users/swang/sites/projects/arts
-    quasar upgrade -i |tee $logFile 2>&1
-fi
-
 logFile="/Users/swang/tmp/qupd-yali.log"
-if [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
-else
+if [ "$1" == "upd" ]; then
     cd /Users/swang/sites/projects/yali
+    echo "  ✅ -- Upgrading yali"
     quasar upgrade -i |tee $logFile 2>&1
+elif [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
 fi
 
 logFile="/Users/swang/tmp/qupd-golf.log"
-if [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
-else
+if [ "$1" == "upd" ]; then
     cd /Users/swang/sites/projects/golf
+    echo "  ✅ -- Upgrading golf"
     quasar upgrade -i |tee $logFile 2>&1
+elif [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
 fi
 
 logFile="/Users/swang/tmp/qupd-apps.log"
-if [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
-else
+if [ "$1" == "upd" ]; then
     cd /Users/swang/sites/projects/apps
+    echo "  ✅ -- Upgrading apps"
     quasar upgrade -i |tee $logFile 2>&1
+elif [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
 fi
