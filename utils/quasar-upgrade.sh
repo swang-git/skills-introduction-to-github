6c30de1ec9fce7ea0b1d40361063f85819c4769f
swang-git <swang71@comcast.net>
2026-09-09 20:48:05 -0400

    check if already upgrade

--- utils/quasar-upgrade.sh
+++ utils/quasar-upgrade.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
-cd /Users/swang/sites/projects/arts
-quasar upgrade -i |tee  ~/tmp/qupd-arts.log 2>&1
 
-cd /Users/swang/sites/projects/yali
-quasar upgrade -i |tee  ~/tmp/qupd-yali.log 2>&1
+logFile="/Users/swang/tmp/qupd-arts.log"
+if [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
+else
+    cd /Users/swang/sites/projects/arts
+    quasar upgrade -i |tee  ~/tmp/qupd-arts.log 2>&1
+fi
 
-cd /Users/swang/sites/projects/golf
-quasar upgrade -i |tee  ~/tmp/qupd-golf.log 2>&1
+logFile="/Users/swang/tmp/qupd-yali.log"
+if [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
+else
+    cd /Users/swang/sites/projects/yali
+    quasar upgrade -i |tee  ~/tmp/qupd-yali.log 2>&1
+fi
 
-cd /Users/swang/sites/projects/apps
-quasar upgrade -i |tee  ~/tmp/qupd-apps.log 2>&1
+logFile="/Users/swang/tmp/qupd-golf.log"
+if [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
+else
+    cd /Users/swang/sites/projects/golf
+    quasar upgrade -i |tee  ~/tmp/qupd-golf.log 2>&1
+fi
+
+logFile="/Users/swang/tmp/qupd-apps.log"
+if [ -f "$logFile" ]; then
+    echo "$logFile exists -- which already upgraded, exit..."
+else
+    cd /Users/swang/sites/projects/apps
+    quasar upgrade -i |tee  ~/tmp/qupd-apps.log 2>&1
+fi
