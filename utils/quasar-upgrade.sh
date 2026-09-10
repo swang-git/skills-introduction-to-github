bc67e4d5ef6f4e3fdc43ef7de4fa613ecab3f671
swang-git <swang71@comcast.net>
2026-09-10 12:59:49 -0400

    dirs based OS

--- utils/quasar-upgrade.sh
+++ utils/quasar-upgrade.sh
@@ -2,35 +2,43 @@
 
 [[ "$1" == "-h" ]] && echo "Usage: qupd check for checking quasar upgrade arts only; upd update all" && exit
 
-logFile="/Users/swang/tmp/qupd-arts.log"
+HOME_DIR="/Users/swang"
+PROJECTS_DIR="/Users/swang/sites/projects"
+OS=$(uname -s)
+if [ "$OS" == "Linux" ]; then
+    HOME_DIR="/home/swang"
+    PROJECTS_DIR="/sites/projects"
+fi
+
+logFile="$HOME_DIR/tmp/qupd-arts.log"
 if [[ "$1" == "check" ]]; then
-    cd ~/sites/projects/arts
-    echo "  ✅ -- Upgrading arts"
+    cd $PROJECTS_DIR/arts
+    echo "  ✅ -- Upgrading $PROJECTS_DIR/arts"
     quasar upgrade -i | tee $logFile 2>&1
 fi
 
-logFile="/Users/swang/tmp/qupd-yali.log"
+logFile="$HOME_DIR/tmp/qupd-yali.log"
 if [ "$1" == "upd" ]; then
-    cd /Users/swang/sites/projects/yali
-    echo "  ✅ -- Upgrading yali"
+    cd $PROJECTS_DIR/yali
+    echo "  ✅ -- Upgrading $PROJECTS_DIR/yali"
     quasar upgrade -i |tee $logFile 2>&1
 elif [ -f "$logFile" ]; then
     echo "$logFile exists -- which already upgraded, exit..."
 fi
 
-logFile="/Users/swang/tmp/qupd-golf.log"
+logFile="$HOME_DIR/tmp/qupd-golf.log"
 if [ "$1" == "upd" ]; then
-    cd /Users/swang/sites/projects/golf
-    echo "  ✅ -- Upgrading golf"
+    cd $PROJECTS_DIR/golf
+    echo "  ✅ -- Upgrading $PROJECTS_DIR/golf"
     quasar upgrade -i |tee $logFile 2>&1
 elif [ -f "$logFile" ]; then
     echo "$logFile exists -- which already upgraded, exit..."
 fi
 
-logFile="/Users/swang/tmp/qupd-apps.log"
+logFile="$HOME_DIR/tmp/qupd-apps.log"
 if [ "$1" == "upd" ]; then
-    cd /Users/swang/sites/projects/apps
-    echo "  ✅ -- Upgrading apps"
+    cd $PROJECTS_DIR/apps
+    echo "  ✅ -- Upgrading $PROJECTS_DIR/apps"
     quasar upgrade -i |tee $logFile 2>&1
 elif [ -f "$logFile" ]; then
     echo "$logFile exists -- which already upgraded, exit..."
