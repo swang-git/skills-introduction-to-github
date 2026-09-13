989d9cc4b54c01713e8c09fa7b13be55e0b28bb2
swang-git <swang71@comcast.net>
2026-09-13 13:50:40 -0400

    enhanced PlatformDataPad.vue

--- apps/src/components/PlatformDataPad.vue
+++ apps/src/components/PlatformDataPad.vue
@@ -1,46 +1,52 @@
 <template>
   <q-dialog v-model="opened" maximized>
     <q-layout class="q-pa-md bg-teal-10 text-cyan-2 text-h6" v-close-popup>
-      <q-card flat class="bg-teal-10 text-white text-h6 justify-center" style="border:cyan 2px solid; border-radius:4%">
-        <ShadowBox class="float-right" style="border-radius:50%;border:yellow 2px solid" :val="compVer" />
-        <tbody class="text-h6">
-          <!-- <tr v-for="(value, prop) in $q.platform.is" :key="prop"><td class="text-right q-px-sm">{{ prop.toUpperCase() }}:</td><td>{{ value }}</td></tr> -->
-          <tr v-for="(value, prop) in Platform.is" :key="prop"><td class="text-right q-px-sm">{{ prop.toUpperCase() }}:</td><td>{{ value }}</td></tr>
-          <tr><td class="text-right q-px-md">Screen Width</td><td>{{ screenwidth }}</td></tr>
-          <tr><td class="text-right q-px-md">Screen Height</td><td>{{ screenheight }}</td></tr>
-          <tr><td class="text-right q-px-md">iPhone</td><td>{{ iPhone }}</td></tr>
-          <tr><td class="text-right q-px-md">iPhone13</td><td>{{ iPhone13 }}</td></tr>
-          <tr><td class="text-right q-px-md">isMate</td><td>{{ isMate }}</td></tr>
+      <q-card flat class="bg-teal-10 text-white text-h6 justify-center" style="border:cyan 2px solid; border-radius:0%">
+        <ShadowBox class="float-right" style="margin:-8px -5px 0 0;border-radius:50%;border:yellow 2px solid" :val="compVer" />
+        <tbody v-if="isDesk" class="text-h6">
+          <tr v-for="(chunk, idx) in platformChunks" :key="idx">
+            <td class="text-right q-px-sm">{{ chunk[0][0] }}:</td>
+            <td>{{ chunk[0][1] }}</td>
+
+            <template v-if="chunk[1]">
+              <!-- <td class="q-pl-xl text-right q-px-sm">{{ chunk[1][0].toUpperCase() }}:</td> -->
+              <td class="q-pl-xl text-right q-px-sm">{{ chunk[1][0] }}:</td>
+              <td>{{ chunk[1][1] }}</td>
+            </template>
+            <template v-else>
+              <td></td>
+              <td></td>
+            </template>
+          </tr>
+        </tbody>
+        <tbody v-else class="text-h6">
+          <tr v-for="(value, prop) in $q.platform.is" :key="prop"><td class="text-right q-px-sm">{{ prop.toUpperCase() }}:</td><td>{{ value }}</td></tr>
         </tbody>
       </q-card>
-
-      <div class="q-mt-md">
-        The device <strong>{{ touch }}</strong> touch capability.
-      </div>
-      <div v-if="isLinux" class="q-mt-md">isLinux <strong>{{ isLinux }}</strong></div>
-      <div v-else-if="isMac" class="q-mt-md">isMac <strong>{{ isMac }}</strong></div>
-      <div class="row justify-center"> Browser User Agent: {{ $q.platform.userAgent }}</div>
-      <div class="row justify-center"> Browser User Agent: {{ UA }}</div>
+      <q-card flat class="q-mt-xs bg-teal-10 text-white text-h6 justify-center" style="border:cyan 2px solid; border-radius:0%">
+        <tr><td class="text-right q-px-md">Screen Width</td><td>{{ screenwidth }}</td></tr>
+        <tr><td class="text-right q-px-md">Screen Height</td><td>{{ screenheight }}</td></tr>
+        <tr><td class="text-right q-px-md">Touch Screen</td><td>The device <strong>{{ touch }}</strong> touch capability.</td></tr>
+        <tr><td class="text-right q-px-md">Browser User Agent</td><td>{{ $q.platform.userAgent }}</td></tr>
+      </q-card>
     </q-layout>
   </q-dialog>
 </template>
 
 <script setup>
 import { computed, ref, onMounted } from "vue";
-import { useQuasar, Platform } from "quasar";
-// import { libFunctions } from "../composables/libFunctions"
+import { Platform } from "quasar";
+import { libFunctions } from "../composables/libFunctions"
+const { screenwidth, screenheight, $q, isDesk } = libFunctions()
 import ShadowBox from "../components/ShadowBox.vue"
 const compVer = computed(() => { return import.meta.env.VITE_BUILD_TAG })
-
-const $q = useQuasar();
 const touch = computed(() => ($q.platform.has.touch ? "has" : "does not have"));
-// const { ENV_DEV } = libFunctions()
 
 const opened = ref(false);
-defineExpose({ openIt });
 const isLinux = ref(false)
 const isMac = ref(false)
 const UA = ref(null)
+defineExpose({ openIt });
 
 onMounted(() => {
   // Evaluate only after browser mounts
@@ -49,6 +55,16 @@ onMounted(() => {
   UA.value = navigator.userAgent
   console.log({ isLinux: isLinux.value, isMac: isMac.value })
 })
+console.log(`-ST-PlatformDataPad`, Platform.is)
+
+// turn object to sorted array [ [prop,value], ... ]
+const entries = Object.entries(Platform.is).sort(([a], [b]) => a.localeCompare(b))
+
+// chunk array into groups of 2
+const platformChunks = []
+for (let i = 0; i < entries.length; i += 2) {
+  platformChunks.push(entries.slice(i, i + 2))
+}
 
 function openIt() {
   opened.value = true;
