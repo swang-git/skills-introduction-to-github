45938253e2a907b68b3599d6e1f946d964c34d8f
swang-git <swang71@comcast.net>
2026-09-26 13:22:06 -0400

    checking exit status to move to next step

--- utils/make-all.sh
+++ utils/make-all.sh
@@ -8,12 +8,43 @@ envx=$1
 
 #cd /Users/swang/sites/projects/arts
 make-quasar $envx arts |tee  ~/tmp/makeall-$envx-arts.log 2>&1
+ret=$?
+# echo $ret
+# exit 0
+
+if [ $ret -eq 0 ]; then
+  echo "✅ make-quasar $envx arts finished successfully"
+else
+  echo "❌ make-quasar $envx arts failed, exit code $ret"
+  exit $ret
+fi
 
 #cd /Users/swang/sites/projects/yali
 make-quasar $envx yali |tee ~/tmp/makeall-$envx-yali.log 2>&1
+ret=$?
+if [ $ret -eq 0 ]; then
+  echo "✅ make-quasar $envx yali finished successfully"
+else
+  echo "❌ make-quasar $envx yali failed, exit code $ret"
+  exit $ret
+fi
 
 #cd /Users/swang/sites/projects/golf
 make-quasar $envx golf |tee ~/tmp/makeall-$envx-golf.log 2>&1
+ret=$?
+if [ $ret -eq 0 ]; then
+  echo "✅ make-quasar $envx golf finished successfully"
+else
+  echo "❌ make-quasar $envx golf failed, exit code $ret"
+  exit $ret
+fi
 
 #cd /Users/swang/sites/projects/apps
 make-quasar $envx apps |tee ~/tmp/makeall-$envx-apps.log 2>&1
+ret=$?
+if [ $ret -eq 0 ]; then
+  echo "✅ make-quasar $envx apps finished successfully"
+else
+  echo "❌ make-quasar $envx apps failed, exit code $ret"
+  exit $ret
+fi
--- utils/quasar-upgrade.sh
+++ utils/quasar-upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ "$1" == "-h" ]] && echo "Usage: 'qupd check': checking/upgrade arts only; 'qupd upd_rest' upgrade yali/golf/apps; 'qupd upd_all' upgrade all" && exit
+[[ "$1" == "-h" ]] && echo "Usage: 'qupd', if if there is new update for art it will continue to upgrade for yali/golf/apps" && exit
 
 HOME_DIR="/Users/swang"
 PROJECTS_DIR="/Users/swang/sites/projects"
@@ -11,43 +11,99 @@ if [ "$OS" == "Linux" ]; then
 fi
 
 logFile="$HOME_DIR/tmp/qupd-arts.log"
-if [[ "$1" == "check" ]]; then
-    cd $PROJECTS_DIR/arts
-    echo "  ✅ -- Upgrading $PROJECTS_DIR/arts"
-    quasar upgrade -i | tee $logFile 2>&1
+cd $PROJECTS_DIR/arts
+quasar upgrade 2>&1 | tee $logFile
+if cat "$logFile" | grep -qi "Congrats"; then
+    echo "ℹ️ Already latest version, skip install/build"
+    exit 0
+elif cat "$logFile" | grep -qi "install"; then
+    # echo "$output"
+    echo "✅ Package arts upgraded"
+else
+    echo "❌ Upgrade error detected:"
+    echo "$logFile"
+    exit 1
 fi
-
-if [ "$1" == "upd_all" ]; then
-    cd $PROJECTS_DIR/arts
-    echo "  ✅ -- Upgrading $PROJECTS_DIR/art"
-    quasar upgrade -i |tee $logFile 2>&1
-elif [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
-fi
-
 logFile="$HOME_DIR/tmp/qupd-yali.log"
-if [ "$1" == "upd_rest" -o "$1" == "upd_all" ]; then
-    cd $PROJECTS_DIR/yali
-    echo "  ✅ -- Upgrading $PROJECTS_DIR/yali"
-    quasar upgrade -i |tee $logFile 2>&1
-elif [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
+cd $PROJECTS_DIR/yali
+quasar upgrade 2>&1 | tee $logFile
+if cat "$logFile" | grep -qi "Congrats"; then
+    echo "ℹ️ Already latest version, skip install/build"
+    exit 0
+elif cat "$logFile" | grep -qi "install"; then
+    # echo "$output"
+    echo "✅ Package arts upgraded"
+else
+    echo "❌ Upgrade error detected:"
+    echo "$logFile"
+    exit 1
 fi
-
 logFile="$HOME_DIR/tmp/qupd-golf.log"
-if [ "$1" == "upd_rest" -o "$1" == "upd_all" ]; then
-    cd $PROJECTS_DIR/golf
-    echo "  ✅ -- Upgrading $PROJECTS_DIR/golf"
-    quasar upgrade -i |tee $logFile 2>&1
-elif [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
+cd $PROJECTS_DIR/golf
+quasar upgrade 2>&1 | tee $logFile
+if cat "$logFile" | grep -qi "Congrats"; then
+    echo "ℹ️ Already latest version, skip install/build"
+    exit 0
+elif cat "$logFile" | grep -qi "install"; then
+    # echo "$output"
+    echo "✅ Package arts upgraded"
+else
+    echo "❌ Upgrade error detected:"
+    echo "$logFile"
+    exit 1
+fi
+logFile="$HOME_DIR/tmp/qupd-apps.log"
+cd $PROJECTS_DIR/apps
+quasar upgrade 2>&1 | tee $logFile
+if cat "$logFile" | grep -qi "Congrats"; then
+    echo "ℹ️ Already latest version, skip install/build"
+    exit 0
+elif cat "$logFile" | grep -qi "install"; then
+    # echo "$output"
+    echo "✅ Package arts upgraded"
+else
+    echo "❌ Upgrade error detected:"
+    echo "$logFile"
+    exit 1
 fi
 
-logFile="$HOME_DIR/tmp/qupd-apps.log"
-if [ "$1" == "upd_rest" -o "$1" == "upd_all" ]; then
-    cd $PROJECTS_DIR/apps
-    echo "  ✅ -- Upgrading $PROJECTS_DIR/apps"
-    quasar upgrade -i |tee $logFile 2>&1
-elif [ -f "$logFile" ]; then
-    echo "$logFile exists -- which already upgraded, exit..."
-fi
+
+# if [[ "$1" == "check" ]]; then
+#     echo "  ✅ -- Upgrading $PROJECTS_DIR/arts"
+#     quasar upgrade -i | tee $logFile 2>&1
+# fi
+
+# if [ "$1" == "upd_all" ]; then
+#     cd $PROJECTS_DIR/arts
+#     echo "  ✅ -- Upgrading $PROJECTS_DIR/art"
+#     quasar upgrade -i |tee $logFile 2>&1
+# elif [ -f "$logFile" ]; then
+#     echo "$logFile exists -- which already upgraded, exit..."
+# fi
+
+# logFile="$HOME_DIR/tmp/qupd-yali.log"
+# if [ "$1" == "upd_rest" -o "$1" == "upd_all" ]; then
+#     cd $PROJECTS_DIR/yali
+#     echo "  ✅ -- Upgrading $PROJECTS_DIR/yali"
+#     quasar upgrade -i |tee $logFile 2>&1
+# elif [ -f "$logFile" ]; then
+#     echo "$logFile exists -- which already upgraded, exit..."
+# fi
+
+# logFile="$HOME_DIR/tmp/qupd-golf.log"
+# if [ "$1" == "upd_rest" -o "$1" == "upd_all" ]; then
+#     cd $PROJECTS_DIR/golf
+#     echo "  ✅ -- Upgrading $PROJECTS_DIR/golf"
+#     quasar upgrade -i |tee $logFile 2>&1
+# elif [ -f "$logFile" ]; then
+#     echo "$logFile exists -- which already upgraded, exit..."
+# fi
+
+# logFile="$HOME_DIR/tmp/qupd-apps.log"
+# if [ "$1" == "upd_rest" -o "$1" == "upd_all" ]; then
+#     cd $PROJECTS_DIR/apps
+#     echo "  ✅ -- Upgrading $PROJECTS_DIR/apps"
+#     quasar upgrade -i |tee $logFile 2>&1
+# elif [ -f "$logFile" ]; then
+#     echo "$logFile exists -- which already upgraded, exit..."
+# fi
